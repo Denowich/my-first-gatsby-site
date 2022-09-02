236cdf18c6c0ebf0006d944d2984bd26ed95181c
Denowich <denowich@gmail.com>
2022-09-02 21:56:42 +0300

    "Finished Gatsby Tutorial Part 2"

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const About = () => {
+  return (
+    <Layout pageTitle='About Me:'>
+      <p>Hello! My name is Den! I'm Web-Developer !</p>
+    </Layout>
+  );
+};
+export const Head = () => <title>About page</title>;
+export default About;
